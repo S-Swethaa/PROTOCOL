c2dc02c193ce2ecedd8a77672c2bc86ea5ff068b
S-Swethaa <146078708+S-Swethaa@users.noreply.github.com>
2025-09-02 18:43:18 +0530

    Update apb_master.v

--- APB PROTOCOL/apb_master.v
+++ APB PROTOCOL/apb_master.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 08/28/2025 09:12:38 PM
+// Create Date: 08/28/2025 10:03:13 PM
 // Design Name: 
-// Module Name: apb_master
+// Module Name: apb_master1
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -13,87 +13,79 @@
 // 
 // Dependencies: 
 // 
-// Revision:
+// Revision:Final
 // Revision 0.01 - File Created
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
 module apb_master(
-//module apb_protocol(
-output reg pwrite,psel,penable,
-output reg [31:0]paddr,pwdata,
-input [31:0]SWDATA,SADDR,
-input preset,pclk,swrite,ptransfer,pready
+    output reg  pwrite, psel, penable,
+    output reg [31:0] paddr, pwdata,prdata,
+    input  [31:0] SWDATA, SADDR,SRDATA,
+    input preset, pclk,
+    input swrite, ptransfer,
+    input pready
 );
+ reg [1:0] state, nextstate;
+ parameter idle=2'b00, setup=2'b01, access=2'b10;
+ always @(posedge pclk) begin
+ if(!preset) begin
+    state   <= idle;
+     psel    <= 0;
+     penable <= 0;
+     pwrite  <= 0;
+     paddr   <= 32'b0;
+     pwdata  <= 32'b0;
+end else begin
+     state <= nextstate;
+end
+end
+
+always @(*) begin
+ nextstate = state;
+ psel = 0;
+ penable = 0;
+ pwrite = 0;
+ paddr = 32'b0;
+ pwdata  = 32'b0;
+
+case(state)
+   idle: begin
+     if(ptransfer) begin
+        nextstate = setup;
+      end else begin
+         nextstate = idle;
+      end
+         end
     
-//reg ptransfer;
-//reg penable;
-//reg [31:0]paddr;
-//reg [31:0]pwdata;
-//reg pready;
-//reg pwrite;
-
-reg [1:0]state,nextstate;
-parameter idle=2'b00,setup=2'b01,access=2'b10;
-
-always @(posedge pclk)begin
-if(!preset)begin
-state<=idle;
-psel<=0;
-//ptransfer<=0;
-penable<=0;
-pwdata<=32'h0;
-paddr<=32'h0;
-end 
-else begin
-state<=nextstate;
-end
-end
-
-always @(*)begin
-case (state)
-//Idle State
- idle:begin
-      psel=0;
-        penable=0;
-    if(ptransfer)begin
-      
-        nextstate=setup;
-        end 
-    else begin
-      nextstate=idle;
-    end 
- end
- 
- //Setup State
- setup:begin
-       penable=0;
-       psel=1;
-       pwrite=swrite;
-       paddr=SADDR;
-       pwdata=SWDATA;
- nextstate=access;
- end
-//Access State
- access:begin 
-        penable=1;
-        if( pready)begin
-           paddr=SADDR;
-           pwdata=SWDATA;
-             if(ptransfer)begin  
-                nextstate=setup;
-             end
-             else
-               nextstate=idle;
+    setup: begin
+      psel= 1;
+      penable = 0;
+      pwrite = swrite;
+      paddr = SADDR;
+      pwdata= SWDATA;
+      nextstate = access;
+      end
+    
+    access: begin
+     psel= 1;
+     penable = 1;
+     pwrite  = swrite;
+     paddr   = SADDR;
+     pwdata  = SWDATA;
+     //prdata  = SRDATA;
+     if(pready) begin
+         if(!swrite)
+           prdata=SRDATA; 
+        if(ptransfer)
+           nextstate = setup;
+        else
+           nextstate = idle;
+        end else begin
+           nextstate = access;
         end
-        else begin
-               nextstate=access;
-        end
-  end
+      end
 endcase
 end
 endmodule
-
 
