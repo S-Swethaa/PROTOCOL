3795f820fd728fd7ec93ac08f8b6e075e0d2747b
S-Swethaa <146078708+S-Swethaa@users.noreply.github.com>
2025-09-02 18:42:50 +0530

    Update apb_slave.v

--- APB PROTOCOL/apb_slave.v
+++ APB PROTOCOL/apb_slave.v
@@ -3,29 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 08/28/2025 09:21:27 PM
-// Design Name: 
-// Module Name: apb_slave
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/28/2025 04:13:54 PM
+// Create Date: 08/28/2025 10:04:45 PM
 // Design Name: 
 // Module Name: spb_slave
 // Project Name: 
@@ -35,56 +13,40 @@
 // 
 // Dependencies: 
 // 
-// Revision:
+// Revision:Final
 // Revision 0.01 - File Created
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
 module spb_slave(
-input pwrite,psel,penable,pclk,preset,
-input [31:0]paddr,pwdata,
-output reg pready,
-output  [31:0] ppwdata);
+input pwrite, psel, penable, pclk, preset,
+input  [31:0] paddr, pwdata,
+output reg   pready,
+output [31:0] prdata);
+    
+reg [31:0] mem [0:31]; 
+reg [31:0] dout;
+assign prdata = dout;
 
-reg [31:0]addr;
-reg [31:0]mem[0:31];
-
-assign ppwdata=mem[paddr[4:0]];
-
-always @(posedge pclk)begin
-if(~preset)begin
-pready=0;
-
- end
+always @(*) begin
+if(!preset) begin
+pready = 0;
+end 
 else begin
-addr=paddr;
-  mem[paddr]=pwdata;
+   if(psel && penable) begin
+      pready = 1;
+        if(pwrite) begin
+           mem[paddr[4:0]] = pwdata; 
+        end
+           else begin
+           dout=mem[paddr[4:0]];  
+        end
+    end 
 
-//slaverr<=0;
-
-/*pready<=1;
+    else begin
+        pready = 0;
+    end
 end
 end
 
-always @(posedge pclk)begin
-addr<=paddr;
-if(psel && ~penable && ~pwrite) begin
-pready=0;
-end
-else if (psel && penable && ~pwrite)begin
-pready=1;    else
-//            pready=0;
-end*/
-if (psel && ~penable && pwrite)begin
-   pready=0;
-   end
-  if (psel && penable && pwrite)begin
-        pready=1;
-      
-      end
-//     
-end
-end
 endmodule
-
